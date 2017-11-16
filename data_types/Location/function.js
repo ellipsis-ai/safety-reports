53baef7a97d09ae23865b74f04fd0fb1ccae6c12
Andrew Catton <>
2017-11-16 16:46:11 -0500

    Try to convince ellipsis to add all the new files

---
+++ data_types/Location/function.js
@@ -0,0 +1,25 @@
+function(ellipsis) {
+  const itemsForText = require('data_type_items_for_text');
+
+ellipsis.success(itemsForText(`
+Default Locations for SSF
+Condor 1
+Condor 2
+Condor Packaging
+Mkt Test Room
+MAP - Farm Ops Room
+MAP - Hallway
+MAP - Tank Room
+MAP - Cold Storage
+Propagation Room
+Seeder Area
+R&D Area
+Open Warehouse
+Old Tralier
+New Trailer
+New Office 1st floor
+New Office 2nd floor
+Truck dock
+Outside Trailer area
+`));
+}
