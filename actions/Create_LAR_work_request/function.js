cb21c911bfe8ba23f7f7f367d903be8e06eca581
Andrew Catton <>
2018-11-06 15:27:56 +0000

    Get the update channels/users right for LAR vs SSF

--- actions/Create_LAR_work_request/function.js
+++ actions/Create_LAR_work_request/function.js
@@ -1,4 +1,8 @@
 function(department, location, description, desiredCompletion, notes, ellipsis) {
   const workRequests = require('work-requests')(ellipsis);
-workRequests.createAndRespond(department, location, description, desiredCompletion, notes);
+
+const followUpUserId = ellipsis.env.LAR_WORK_REQUEST_FOLLOW_UP_USER_ID;
+const updateChannel = ellipsis.env.LAR_WORK_REQUEST_UPDATE_CHANNEL;
+
+workRequests.createAndRespond(department, location, description, desiredCompletion, notes, followUpUserId, updateChannel);
 }
--- actions/Create_SSF_work_request/function.js
+++ actions/Create_SSF_work_request/function.js
@@ -1,4 +1,8 @@
 function(department, location, description, desiredCompletion, notes, ellipsis) {
   const workRequests = require('work-requests')(ellipsis);
-workRequests.createAndRespond(department, location, description, desiredCompletion, notes);
+
+const followUpUserId = ellipsis.env.SSF_WORK_REQUEST_FOLLOW_UP_USER_ID;
+const updateChannel = ellipsis.env.SSF_WORK_REQUEST_UPDATE_CHANNEL;
+
+workRequests.createAndRespond(department, location, description, desiredCompletion, notes, followUpUserId, updateChannel);
 }
