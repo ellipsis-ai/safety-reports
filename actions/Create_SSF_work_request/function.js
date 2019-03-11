8c236648492ec397a1fa9c8652f8e3e374c0517b
Andrew Catton <>
2019-03-11 20:24:51 +0000

    Moving from "desired completion" to urgency for work requests

--- actions/Create_SSF_work_request/function.js
+++ actions/Create_SSF_work_request/function.js
@@ -1,8 +1,8 @@
-function(department, location, description, desiredCompletion, notes, ellipsis) {
+function(department, location, description, urgency, notes, ellipsis) {
   const workRequests = require('work-requests')(ellipsis);
 
 const followUpUserId = ellipsis.env.SSF_WORK_REQUEST_FOLLOW_UP_USER_ID;
 const updateChannel = ellipsis.env.SSF_WORK_REQUEST_UPDATE_CHANNEL;
 
-workRequests.createAndRespond(department, location, description, desiredCompletion, notes, followUpUserId, updateChannel);
+workRequests.createAndRespond(department, location, description, urgency, notes, followUpUserId, updateChannel);
 }
---
+++ data_types/Urgency/function.js
@@ -0,0 +1,8 @@
+function(ellipsis) {
+  ellipsis.success([
+  { id: 'urgent', label: 'Urgent - within 24 hours' },
+  { id: 'high', label: 'High - within 3 days' },
+  { id: 'normal', label: 'Normal - within 5 days' },
+  { id: 'low', label: 'Low - within 10 days' }
+]);
+}
