cf8af60db71b9ef65312e2aad905af5b776a281d
Andrew Catton <>
2019-03-11 20:51:20 +0000

    set priority in fiix

--- data_types/Urgency/function.js
+++ data_types/Urgency/function.js
@@ -1,8 +1,8 @@
 function(ellipsis) {
   ellipsis.success([
-  { id: 'urgent', label: 'Urgent - within 24 hours' },
-  { id: 'high', label: 'High - within 3 days' },
-  { id: 'normal', label: 'Normal - within 5 days' },
-  { id: 'low', label: 'Low - within 10 days' }
+  { id: 'urgent', label: 'Urgent - within 24 hours', fiixLabel: 'Highest' },
+  { id: 'high', label: 'High - within 3 days', fiixLabel: 'High' },
+  { id: 'normal', label: 'Normal - within 5 days', fiixLabel: 'Medium' },
+  { id: 'low', label: 'Low - within 10 days', fiixLabel: 'Low' }
 ]);
 }
