55d74bcdc45645441e1f4c39e5b04351cb8eecef
Luke Andrews <luke@ellipsis.ai>
2019-05-21 19:13:30 +0000

    Add file input to the regular work request creation flows

--- actions/Create_work_request/function.js
+++ actions/Create_work_request/function.js
@@ -1,8 +1,8 @@
-function(department, location, description, urgency, notes, ellipsis) {
+function(department, location, description, urgency, notes, file, ellipsis) {
   const workRequests = require('work-requests')(ellipsis);
 
 const followUpUserId = ellipsis.env.WORK_REQUEST_FOLLOW_UP_USER_ID;
 const updateChannel = ellipsis.env.WORK_REQUEST_UPDATE_CHANNEL;
 
-workRequests.createAndRespond(department, location, description, urgency, notes, followUpUserId, updateChannel);
+workRequests.createAndRespond(department, location, description, urgency, notes, file, followUpUserId, updateChannel);
 }
